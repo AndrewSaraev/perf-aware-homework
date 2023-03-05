53e60d6d43924d22061167f4a34b14daa8eeadef
Andrew Saraev <a.a.saraev@yandex.ru>
2023-03-05 17:59:37 +0000

    Added new listings to decode, with failed results.

---
+++ part_1/listing_0039_more_movs_dec.asm
@@ -0,0 +1,29 @@
+bits 16
+
+mov si, bx
+mov dh, al
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unsupported mov op (mod other than 11)
+; unsupported mov op (mod other than 11)
+; unsupported mov op (mod other than 11)
+; unknown op code
+; unsupported mov op (mod other than 11)
+; unknown op code
+; unsupported mov op (mod other than 11)
+; unknown op code
+; unknown op code
+; unsupported mov op (mod other than 11)
+; unsupported mov op (mod other than 11)
+; unsupported mov op (mod other than 11)
+; unknown op code
---
+++ part_1/listing_0040_challenge_movs_dec.asm
@@ -0,0 +1,36 @@
+bits 16
+
+; unsupported mov op (mod other than 11)
+; unknown op code
+; unsupported mov op (mod other than 11)
+; unknown op code
+; unknown op code
+; unsupported mov op (mod other than 11)
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unsupported mov op (mod other than 11)
+; unknown op code
+; unknown op code
+; unsupported mov op (mod other than 11)
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
+; unknown op code
